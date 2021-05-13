552793ee5fd87bc7855d4741ba0798e04e5e4150
DowelMartin <martindowelofficial@gmail.com>
2021-05-13 18:32:19 +0200

    CSRF & Flask Form Fixes

--- trafficsignsidentifier/__init__.py
+++ trafficsignsidentifier/__init__.py
@@ -1,5 +1,10 @@
+import os
 from flask import Flask, render_template
+from flask_wtf import CSRFProtect
 
 app = Flask(__name__)
+csrf = CSRFProtect(app)
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
 
 from trafficsignsidentifier import routes
